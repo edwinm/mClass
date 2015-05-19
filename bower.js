76489a0067b11fe20df1c392477bf61295e6ae7a
Edwin Martin <edwin@bitstorm.org>
2015-05-19 21:33:23 +0200

    Adding bower.js

---
+++ bower.js
@@ -0,0 +1,17 @@
+{
+    "name": "newClass",
+    "description": "Provides class like programming to JavaScript while staying close to prototypal inheritance.",
+    "version": "1.0.0",
+    "main": "./newclass.js",
+    "ignore": [],
+    "keywords": ["OOP", "object oriented", "prototype", "prototypal", "inheritance"],
+    "authors": [
+        "Edwin Martin <edwin@bitstorm.org>"
+    ],
+    "repository": {
+        "type": "git",
+        "url": "git://github.com/edwinm/newClass.git"
+    },
+    "dependencies": {
+    }
+}
